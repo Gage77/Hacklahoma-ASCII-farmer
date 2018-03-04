8e04812793cbdfdc77b5c5a29218b0f0da91116f
Cmdr Shepard <Conner.G.Flansburg-1@ou.edu>
2018-03-04 01:08:57 -0600

    farther changes to map & plant

--- src/main/java/colonizer/game/Map.java
+++ src/main/java/colonizer/game/Map.java
@@ -17,6 +17,13 @@ public class Map
   private ArrrayList<String> farms = new ArrrayList<String>();
   //character location within the square
   private String characterLocation = "-1";
+  //holds all of the plant locations
+  private dictonary plantValues;
+
+  public static map()
+  {
+    //TODO write, and find way to fill dictonary
+  }
 
   //grows all the plants on the map
   public static void grow()
@@ -29,15 +36,29 @@ public class Map
   }
 
   //draws/update the HUD
-  public static string drawHud(int x, int y)
+  public static string drawHud(int x, int y, char c)
   {
-    return getLocation(x, y);
+    string hud = "X coordinate = "+ x.ToString + " Y coordinate = "
+    +y.ToString + getLocationInfo(x, y, c);
+    return hud;
   }
 
   //gets the info for the position of the player
-  public static string getLocationInfo(int x, y)
+  public static string getLocationInfo(int x, int y, char c)
   {
-    //TODO how do we determine this?
+    //if the coordinate holds a plant, return the plant
+    if(plantValues.ContainsKey(int.Parse(x.ToString() + y.ToString()))
+      return plantValues[int.Parse(x.ToString() + y.ToString().toString];
+    //otherwise return the terrian type
+    else
+    {
+      if(c == '^')
+        return "mountain";
+      if(c == 'W')
+        return "water"
+      if(c == 'O')
+        return "Ice"
+    };
   }
 
   //sets the biome
