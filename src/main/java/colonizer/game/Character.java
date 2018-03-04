059e12980aaadde0998bf704354504ef2acb0389
Cmdr Shepard <Conner.G.Flansburg-1@ou.edu>
2018-03-03 18:08:53 -0600

    added player hud info

--- src/main/java/colonizer/game/Character.java
+++ src/main/java/colonizer/game/Character.java
@@ -7,22 +7,38 @@ import java.util.*;
 
 public class Character
 {
-  private ArrayList<String> inventory;
+  //this will store the player inventory
+  private String [] inventory;
+  //the value (0-100) of the players hunger
   private int hunger;
+  //the point of the character's location
   private Point location;
 
+  //constructor
   public Character()
   {
     hunger = 100;
     location = new Point(0, 0);
-    inventory = new ArrayList<String>();
+    String [] inventory = new int[3];
   }
 
+  //constructor
   public Character(int hunger, int x, int y)
   {
     this.hunger = hunger;
     this.location = new Point(x, y);
+    String [] inventory = new int[3];
   }
+
+  //this will create the string for player HUD
+  public String createHud ()
+  {
+    String hud = new String("Inventory: "+ "\n"
+    +"1.Tools  2.Base 3.Food  "+ "\n"
+    +"Hunger Level "+ hunger+"%")
+  }
+
+  //getters & setters
 
   public int getHunger()
   {
@@ -49,5 +65,22 @@ public class Character
   public void setLocation(int x, int y)
   {
     location.setLocation(x, y);
+  }
+
+  public String [] getInventory()
+  {
+    return inventory;
+  }
+
+  public void addInventory(String item)
+  {
+    if(item.contains("tool"))
+      String[1].concat(item);
+      
+    if(item.contains("base"))
+      String[2].concat(item);
+
+    if(item.contains("food"))
+      String[3].concat(item);
   }
 }
